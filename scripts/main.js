4feed48071699c4787af8052ded0f0ff62a5cbb0
Sam <snippetsbysam@outlook.com>
2021-03-13 22:23:52 +0000

    upload finished projec

---
+++ scripts/main.js
@@ -0,0 +1,53 @@
+let defaultCanvasHeight = 600;
+let defaultCanvasWidth = 600;
+let gridGap = 1;
+let defaultColor = "#FFFFFF";
+let drawColor = "#0000FF";
+const redrawButton = document.querySelector("#redraw");
+
+function randomColor() {
+    return `#${Math.floor(Math.random()*16777215).toString(16)}`;
+}
+
+// Create grid function
+function createDivGrid(gridlength) {
+    gridlength = parseInt(gridlength);
+    if (gridlength > 100 || gridlength < 1 || isNaN(gridlength)) {
+        alert("Grid Length must be a number between  less than 100");
+        return;
+    }
+    const container = document.querySelector('#container');
+    let gridArea = Math.pow(gridlength, 2);
+    // make sure square dimensions are whole numbers
+    let squareHeight = Math.floor(defaultCanvasHeight / gridlength);
+    let squareWidth = Math.floor(defaultCanvasWidth / gridlength);
+    const subDiv = document.createElement('div');
+    subDiv.setAttribute("id", "container");
+    subDiv.setAttribute("class", "grid-container");
+    subDiv.setAttribute("style", `display: flex; flex-wrap: wrap; grid-gap: ${gridGap}px;`);
+    //calculate new canvas dimensions from square dimensions
+    subDiv.style.height = `${(squareHeight * gridlength) + ((gridlength - 1) * gridGap )}px`;
+    subDiv.style.width = `${(squareWidth * gridlength) + ((gridlength - 1) * gridGap )}px`;
+    //subDiv.style.border = "1px solid";
+    
+    for (let i = 0; i < gridArea; i++) {
+        const square = document.createElement('div');
+        square.setAttribute("class", "grid");
+        square.setAttribute("style", `background-color: ${defaultColor}; width:${squareWidth}px; height:${squareHeight}px;`);
+        square.addEventListener("mouseenter", function(e) {e.target.style.backgroundColor = randomColor();})
+        subDiv.appendChild(square);
+    }
+    const parentDiv = container.parentNode;
+    parentDiv.replaceChild(subDiv, container);
+    //container.append(subDiv);
+}
+
+function customGrid() {
+    let newGridSize = prompt("Enter a new Grid size");
+    createDivGrid(newGridSize);
+}
+redrawButton.addEventListener('click', customGrid)
+createDivGrid(16);
+//square.document.createElement('div')
+//squares = Array(16).fill(square);
+//container.append(subDiv);
